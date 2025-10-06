7b8ce0f8cfd1333cc2eab5725c04ba2cabaceb46
Kimyudin <163673413+KimYujin22@users.noreply.github.com>
2025-10-07 04:13:56 +0900

    [Init] 초기 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <Router>
-      <Routes>{/* 나중에 페이지 Route 추가 */}</Routes>
+      <Routes>{/* 나중에 페이지 Route 추가! */}</Routes>
     </Router>
   );
 };
