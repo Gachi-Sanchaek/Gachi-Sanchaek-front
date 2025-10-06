1a23ad1b1e8f9200cc2c05ab7b2d0a20fc9b9dc0
Kimyudin <163673413+KimYujin22@users.noreply.github.com>
2025-10-07 03:15:59 +0900

    [Init] 초기 세팅

---
+++ README.md
@@ -0,0 +1,22 @@
+# 가치 산책
+
+## 🛠 기술 스택
+
+- React + TypeScript
+- Vite
+- TailwindCSS
+- React-Router
+- tanstack query
+- Zustand (상태 관리)
+- Axios (API 요청)
+- tailwindcss
+
+## 🚀 설치 및 실행
+
+```bash
+# 의존성 설치
+pnpm install
+
+# 개발 서버 실행
+pnpm run dev
+```
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>{/* 나중에 페이지 Route 추가 */}</Routes>
+    </Router>
+  );
+};
+
+export default App;
