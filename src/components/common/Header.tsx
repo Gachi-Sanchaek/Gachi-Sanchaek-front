c65bf8c31ceac22174641315b5953b73a59f51e3
somin2352 <somin030502@gmail.com>
2025-10-10 21:14:38 +0900

    [Modify] 헤더 색상 props 추가
    - svg 색상별 추가 및 이름 변경

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,12 +1,14 @@
 import { useNavigate } from 'react-router-dom';
-import ArrowUp from '/src/assets/arrow-up.svg';
+import BackWhite from '/src/assets/back-white.svg';
+import BackBlack from '/src/assets/back-black.svg';
 
 interface HeaderProps {
   hasArrow?: boolean;
   title?: string;
+  titleColor?: 'black' | 'white';
 }
 
-const Header = ({ hasArrow = false, title }: HeaderProps) => {
+const Header = ({ hasArrow = false, title, titleColor = 'white' }: HeaderProps) => {
   const navigate = useNavigate();
 
   const handleClickBack = () => {
@@ -14,18 +16,18 @@ const Header = ({ hasArrow = false, title }: HeaderProps) => {
   };
 
   return (
-    <div className='fixed top-0 w-full max-w-[480px] bg-transparent z-50 py-2.5'>
+    <header className='fixed top-0 w-full max-w-[480px] bg-transparent z-50 py-2.5'>
       {hasArrow ? (
         <div className='flex items-center justify-center px-6'>
           <button type='button' className='absolute left-6 cursor-pointer' onClick={handleClickBack}>
-            <img src={ArrowUp} alt='뒤로가기' className='w-6 h-6' />
+            <img src={titleColor === 'white' ? BackWhite : BackBlack} alt='뒤로가기' className='w-6 h-6' />
           </button>
-          <h1 className='font-[PretendardVariable] font-semibold text-white text-[18px] select-none'>{title}</h1>
+          <h1 className={`font-[PretendardVariable] font-semibold text-${titleColor} text-[18px] select-none`}>{title}</h1>
         </div>
       ) : (
         <h1 className='pl-6 font-[Cafe24Ssurround] font-bold text-[28px] text-white select-none'>가치 산책</h1>
       )}
-    </div>
+    </header>
   );
 };
 
