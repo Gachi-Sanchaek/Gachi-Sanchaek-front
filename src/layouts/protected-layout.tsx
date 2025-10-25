c3b2caa0e3e2d3dc808d8a68d166ae0a2e11997d
somin2352 <somin030502@gmail.com>
2025-10-26 00:48:18 +0900

    [Feat] 레이아웃 헤더 조건 처리

--- src/layouts/protected-layout.tsx
+++ src/layouts/protected-layout.tsx
@@ -1,16 +1,18 @@
 import { matchPath, Outlet, useLocation } from 'react-router-dom';
 import Header from '../components/common/Header';
+import { useCategoryStore } from '../store/useCategoryStore';
 
 const ProtectedLayout = () => {
   const location = useLocation();
-
   const isHome = !!matchPath('/', location.pathname);
+
+  const { selectedCategory } = useCategoryStore();
 
   return (
     <div className='flex justify-center min-h-screen bg-gray-100'>
       <div className={`w-full max-w-[480px] min-h-screen ${isHome ? 'bg-gradient-to-b from-[#5FD59B] to-[#FFEC8A]' : 'bg-white'}`}>
-        <Header />
-        <div className='pt-10'>
+        <Header hasArrow={!isHome} title={selectedCategory} titleColor='black' bgColor={isHome ? 'transparent' : 'white'} />
+        <div className='pt-12'>
           <Outlet />
         </div>
       </div>
