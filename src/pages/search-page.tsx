129481c55a9c049db9296cc727247f71e1889403
somin2352 <somin030502@gmail.com>
2025-10-31 17:50:24 +0900

    [Feat] 일반 산책 환경일 경우 바텀시트 숨김 처리

--- src/pages/search-page.tsx
+++ src/pages/search-page.tsx
@@ -1,8 +1,11 @@
 import BottomSheet from '../components/SearchPage/BottomSheet/BottomSheet';
 import Category from '../components/SearchPage/Category/Category';
 import KakaoMap from '../components/SearchPage/KakaoMap';
+import { useCategoryStore } from '../store/useCategoryStore';
 
 export default function SearchPage() {
+  const { selectedCategory } = useCategoryStore();
+
   return (
     <div>
       {/* base layer */}
@@ -10,7 +13,7 @@ export default function SearchPage() {
         <Category />
         <KakaoMap />
       </div>
-      <BottomSheet />
+      {selectedCategory !== '산책' && <BottomSheet />}
     </div>
   );
 }
