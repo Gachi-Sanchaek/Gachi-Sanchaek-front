b623ebda3d9d95aea3053bf72a40ee65751a9371
somin2352 <somin030502@gmail.com>
2025-11-10 23:33:19 +0900

    [Feat] 플로깅 인증 api 연결을 위한 타입 및 api 함수 정의

--- src/apis/walk-auth.ts
+++ src/apis/walk-auth.ts
@@ -1,10 +1,25 @@
-import type { QrAuthResponse } from '../types/walk-auth';
+import type { PloggingAuthRequest, PloggingAuthResponse, QrAuthResponse } from '../types/walk-auth';
 import { axiosInstance } from './axios';
 
 export const postQrAuth = async (qrToken: string): Promise<QrAuthResponse> => {
   const { data } = await axiosInstance.post(`/api/v1/walk/qr`, null, {
     params: { qrToken },
   });
+
+  return data;
+};
+
+export const postPloggingAuth = async ({ walkId, image }: PloggingAuthRequest): Promise<PloggingAuthResponse> => {
+  const { data } = await axiosInstance.post(
+    `/api/v1/walk/plogging`,
+    { image },
+    {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+      params: walkId,
+    }
+  );
 
   return data;
 };
