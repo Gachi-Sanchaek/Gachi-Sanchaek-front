b64751de9f7721fc085020aefb1bf5f4cacd0b5e
Jaeyeon-23 <reenact115@gmail.com>
2025-11-13 01:54:53 +0900

    [Feat] 산책 종료 UI 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,32 @@
-import { type RouteObject, createBrowserRouter, RouterProvider } from 'react-router-dom';
-import LogIn from './pages/login-page';
-import Home from './pages/home-page';
-import PublicLayout from './layouts/public-layout';
-import ProtectedLayout from './layouts/protected-layout';
-import SearchPage from './pages/search-page';
-import SignUp from './pages/signup-page';
-import WalkPage from './pages/walk-page';
-import WalkRoutePage from './pages/walk-route-page';
-import { KAKAO_OAUTH } from './utils/kakao-constants';
-import { KakaoOAuthHandler } from './pages/kakao-oauth-handler';
-import MyPage from './pages/my-page';
-import RankingPage from './pages/ranking-page';
-import QRAuthPage from './pages/qr-auth-page';
-import WalkRealtimePage from './pages/walk-realtime-page';
-import PloggingAuthPage from './pages/plogging-auth-page';
+import {
+  type RouteObject,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import LogIn from "./pages/login-page";
+import Home from "./pages/home-page";
+import PublicLayout from "./layouts/public-layout";
+import ProtectedLayout from "./layouts/protected-layout";
+import SearchPage from "./pages/search-page";
+import SignUp from "./pages/signup-page";
+import WalkPage from "./pages/walk-page";
+import WalkRoutePage from "./pages/walk-route-page";
+import { KAKAO_OAUTH } from "./utils/kakao-constants";
+import { KakaoOAuthHandler } from "./pages/kakao-oauth-handler";
+import MyPage from "./pages/my-page";
+import RankingPage from "./pages/ranking-page";
+import QRAuthPage from "./pages/qr-auth-page";
+import WalkRealtimePage from "./pages/walk-realtime-page";
+import PloggingAuthPage from "./pages/plogging-auth-page";
+import WalkEndPage from "./pages/walk-end-page";
 
 const publicRoutes: RouteObject[] = [
   {
-    path: '/',
+    path: "/",
     element: <PublicLayout />,
     children: [
-      { path: 'login', element: <LogIn /> },
-      { path: 'signup', element: <SignUp /> },
+      { path: "login", element: <LogIn /> },
+      { path: "signup", element: <SignUp /> },
       { path: KAKAO_OAUTH, element: <KakaoOAuthHandler /> },
     ],
   },
@@ -29,18 +34,19 @@ const publicRoutes: RouteObject[] = [
 
 const protectedRoutes: RouteObject[] = [
   {
-    path: '/',
+    path: "/",
     element: <ProtectedLayout />,
     children: [
       { index: true, element: <Home /> },
-      { path: 'search', element: <SearchPage /> },
-      { path: 'mypage', element: <MyPage /> },
-      { path: 'walk', element: <WalkPage /> },
-      { path: 'walk/route', element: <WalkRoutePage /> },
-      { path: 'ranking', element: <RankingPage /> },
-      { path: 'walk/realtime', element: <WalkRealtimePage /> },
-      { path: 'qr-auth', element: <QRAuthPage /> },
-      { path: 'plogging-auth', element: <PloggingAuthPage /> },
+      { path: "search", element: <SearchPage /> },
+      { path: "mypage", element: <MyPage /> },
+      { path: "walk", element: <WalkPage /> },
+      { path: "walk/route", element: <WalkRoutePage /> },
+      { path: "ranking", element: <RankingPage /> },
+      { path: "walk/realtime", element: <WalkRealtimePage /> },
+      { path: "qr-auth", element: <QRAuthPage /> },
+      { path: "plogging-auth", element: <PloggingAuthPage /> },
+      { path: "end", element: <WalkEndPage /> },
     ],
   },
 ];
