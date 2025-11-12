34acfb912b3d809a98ecd3a13fa7cdbbf07ffd8a
Kimyudin <163673413+KimYujin22@users.noreply.github.com>
2025-11-13 06:45:14 +0900

    [Feat] 프로필 및 랭킹 API를 zustand store로 관리 변경; types, apis 업데이트

---
+++ src/apis/user.ts
@@ -0,0 +1,45 @@
+import { axiosInstance } from "./axios";
+import type {
+  UserProfileRequest,
+  UserProfileResponse,
+  UserRankingResponse,
+} from "../types/user";
+import { useUserStore } from "../store/UserStore";
+
+export const getUserProfile = async (): Promise<UserProfileResponse> => {
+  const response = await axiosInstance.get(`/api/v1/users/me`);
+  console.log(response);
+
+  return response.data.data;
+};
+
+export const getUserRanking = async (
+  weekDate: string
+): Promise<UserRankingResponse> => {
+  const response = await axiosInstance.get(`/api/v1/rankings/my-ranking`, {
+    params: { date: weekDate },
+  });
+  console.log(response);
+
+  return response.data.data;
+};
+
+export const updateUserProfile = async (data: Partial<UserProfileRequest>) => {
+  const store = useUserStore.getState();
+
+  if (!store.profile) {
+    throw new Error("프로필 정보가 아직 로드되지 않았습니다.");
+  }
+
+  const payload: UserProfileRequest = {
+    nickname: data.nickname ?? store.profile?.nickname,
+    profileImageUrl: data.profileImageUrl ?? store.profile?.profileImageUrl,
+  };
+
+  const { data: response } = await axiosInstance.patch<UserProfileResponse>(
+    "/api/v1/users/me",
+    payload
+  );
+
+  return response;
+};
