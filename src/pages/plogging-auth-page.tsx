8ab619264e357554d067c492de3589880fd38861
somin2352 <somin030502@gmail.com>
2025-11-14 23:33:20 +0900

    [Modify] 로딩중 모달창 위치 수정

--- src/pages/plogging-auth-page.tsx
+++ src/pages/plogging-auth-page.tsx
@@ -105,7 +105,6 @@ export default function PloggingAuthPage() {
           setShowSuccessModal(true);
         } else {
           alert(data.data.message);
-          location.reload();
         }
       } catch (e) {
         alert('접속이 원활하지 않습니다. 잠시 후 다시 시도해 주세요.');
@@ -142,19 +141,6 @@ export default function PloggingAuthPage() {
       console.error('plogging auth error', e);
     }
   };
-
-  if (isAiDetecting) {
-    return (
-      <Modal
-        title={
-          <div className='flex flex-col justify-center items-center'>
-            <img src={SirenBonggong} alt='ai detecting' />
-            <p className='pt-3'>AI가 검증하고 있어요!</p>
-          </div>
-        }
-      />
-    );
-  }
 
   return (
     <div className='relative w-full h-[100dvh] flex flex-col justify-center items-center gap-6 bg-black'>
@@ -199,7 +185,7 @@ export default function PloggingAuthPage() {
         />
       )}
 
-      {showSubmitModal && capturedImage && (
+      {showSubmitModal && capturedImage && !isAiDetecting && (
         <Modal
           title={
             <>
@@ -231,6 +217,16 @@ export default function PloggingAuthPage() {
               onClick: () => handleSuccess(),
             },
           ]}
+        />
+      )}
+      {isAiDetecting && (
+        <Modal
+          title={
+            <div className='flex flex-col justify-center items-center'>
+              <img src={SirenBonggong} alt='ai detecting' />
+              <p className='pt-3'>AI가 검증하고 있어요!</p>
+            </div>
+          }
         />
       )}
     </div>
